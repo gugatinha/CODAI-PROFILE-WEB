0017186d9e490122ea1584202820b0d6dbd64f7f
Laura Fernades <lauraifernandes2@gmail.com>
2021-02-22 12:59:13 -0300

    Primeiro commit

---
+++ js/index.js
@@ -0,0 +1,62 @@
+var nome = "Laura Fernandes.";
+var cargo = "Desenvolvedora."
+var nomehtml = document.getElementById("nome-no-html");
+var cargohtml = document.getElementById("cargo-no-html");
+var texto1 = document.getElementById("texto-1");
+var texto2 = document.getElementById("texto-2");
+
+/*function dizerola()
+{
+
+    alert("Olá Laura!")
+
+}*/
+
+function colocarnomenohtml(nome)
+{
+
+nomehtml.innerHTML = nome;
+
+}
+
+function colocarcargonohtml(cargo)
+{
+
+cargohtml.innerHTML = cargo;
+
+}
+
+function logarnome()
+{
+
+    console.log(nome);
+
+}
+
+function clicknoprojetos()
+{
+
+    console.log("clicou no botao projetos");
+    texto1.style.display = "none";
+    texto2.style.display = "block";
+    
+}
+
+function clicknosobre()
+{
+
+    console.log("clicou no botao sobre");
+    texto1.style.display = "block";
+    texto2.style.display = "none";
+
+}
+
+colocarnomenohtml(nome);
+
+colocarcargonohtml(cargo);
+
+
+
+/*logarnome();*/
+
+/*dizerola();*/
